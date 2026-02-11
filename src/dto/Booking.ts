d7144781025309f5ff988f4866ed2aa3155f0a2e
quangkid2212 <163752892+hckq2212@users.noreply.github.com>
2026-02-11 14:30:16 +0700

    update tạm

--- src/dto/Booking.ts
+++ src/dto/Booking.ts
@@ -39,7 +39,7 @@ export class Booking {
     @Column({ type: "decimal", precision: 15, scale: 2, nullable: false })
     total_price: number;
 
-    @Column({ nullable: false, default: 'NOT_CHECKED_IN' })
+    @Column({ nullable: true })
     status: string;
 
     @Column({ nullable: false })
