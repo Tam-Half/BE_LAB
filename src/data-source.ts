75ed881e9e88fc0065f6e68f9582f71699d41108
quangkid2212 <163752892+hckq2212@users.noreply.github.com>
2026-05-10 09:55:18 +0700

    Update deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -26,13 +26,22 @@ import { ExtraService } from "./dto/ExtraService"
 import { Shift } from "./dto/Shift"
 dotenv.config()
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    ...(isProduction
+        ? {
+            url: process.env.DB_URL,
+            ssl: { rejectUnauthorized: false },
+          }
+        : {
+            host: process.env.DB_HOST,
+            port: 5432,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+          }),
     synchronize: true,
     logging: false,
     entities: [User,
